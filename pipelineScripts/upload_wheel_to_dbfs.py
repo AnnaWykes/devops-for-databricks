93178ab9b399a16c492cdbb74a83b1d2d02a5afb
Anna Wykes <wykesanna32@googlemail.com>
2021-05-26 13:20:41 +0100

    Update upload_wheel_to_dbfs.py

--- pipelineScripts/upload_wheel_to_dbfs.py
+++ pipelineScripts/upload_wheel_to_dbfs.py
@@ -3,10 +3,10 @@ import os
 
 DBRKS_REQ_HEADERS = {
     'Authorization': 'Bearer ' + os.environ['DBRKS_BEARER_TOKEN'],
-    'X-Databricks-Azure-Workspace-Resource-Id': '/subscriptions/6392d180-7beb-4851-8c38-8d32bb52555f/resourceGroups/devopsfordatabricks/providers/Microsoft.Databricks/workspaces/devopsfordatabricks_dbx',
+    'X-Databricks-Azure-Workspace-Resource-Id': '/subscriptions/[subscriptionid]/resourceGroups/devopsfordatabricks/providers/Microsoft.Databricks/workspaces/[workspacename]',
     'X-Databricks-Azure-SP-Management-Token': os.environ['DBRKS_MANAGEMENT_TOKEN']}
 
-dbrks_rest_url = "https://adb-3843427314946365.5.azuredatabricks.net/api/2.0/dbfs/put"
+dbrks_rest_url = "https://[databricks instance].azuredatabricks.net/api/2.0/dbfs/put"
 
 wheel_loaction = os.environ['DefaultWorkingDirectory'] + '/wheel/dist/dfordbx-0.0.1-py3-none-any.whl'
 print(wheel_loaction)
