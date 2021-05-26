582162079b21f7597d2cc0ea92a8af32b94b005e
Anna Wykes <wykesanna32@googlemail.com>
2021-05-26 13:13:12 +0100

    Added refs to changeable properties

--- pipelineScripts/install_wheel.py
+++ pipelineScripts/install_wheel.py
@@ -4,15 +4,15 @@ import os
 
 DBRKS_REQ_HEADERS = {
     'Authorization': 'Bearer ' + os.environ['DBRKS_BEARER_TOKEN'],
-    'X-Databricks-Azure-Workspace-Resource-Id': '/subscriptions/6392d180-7beb-4851-8c38-8d32bb52555f/resourceGroups/devopsfordatabricks/providers/Microsoft.Databricks/workspaces/devopsfordatabricks_dbx',
+    'X-Databricks-Azure-Workspace-Resource-Id': '/subscriptions/[subscriptionId]/resourceGroups/devopsfordatabricks/providers/Microsoft.Databricks/workspaces/[workspace name]',
     'X-Databricks-Azure-SP-Management-Token': os.environ['DBRKS_MANAGEMENT_TOKEN']}
 
-DBRKS_REQ_BODY = {'cluster_id': '0423-124042-plugs864', 'libraries': [{'whl': 'dbfs:/wheels/dfordbx-0.0.1-py3-none-any.whl'}]}
+DBRKS_REQ_BODY = {'cluster_id': '[clusterid]', 'libraries': [{'whl': 'dbfs:/wheels/dfordbx-0.0.1-py3-none-any.whl'}]}
 
 DBRKS_INSTALL_ENDPOINT = 'api/2.0/libraries/install'
 
 response = requests.post(
-    "https://adb-3843427314946365.5.azuredatabricks.net/" + DBRKS_INSTALL_ENDPOINT,
+    "https://[databricks instance].azuredatabricks.net/" + DBRKS_INSTALL_ENDPOINT,
     headers=DBRKS_REQ_HEADERS,
     json=DBRKS_REQ_BODY)
 
